f2b031f4ce81c5263811970b93237870831d6d11
kraiviks <kraiviks@gmail.com>
2021-01-30 19:33:28 +0200

    add lazy-load-img

--- dist/precache-manifest.1ca11117dcddac700c9b3e141d450b5b.js
+++ dist/precache-manifest.1ca11117dcddac700c9b3e141d450b5b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b1afe3d6198244c354d0",
+    "revision": "a4faad2e534a2d8e1714",
     "url": "/serhii_k/css/about.01cf6fd8.css"
   },
   {
-    "revision": "cd8b362e446bdfa7cdb5",
+    "revision": "742110297c97248e111b",
     "url": "/serhii_k/css/app.8953980f.css"
   },
   {
@@ -16,40 +16,64 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/serhii_k/img/Avatar.52e17c31.png"
   },
   {
+    "revision": "8663d62c3856a1f8cf3a09696e2521fc",
+    "url": "/serhii_k/img/H-Tecnology-min.8663d62c.png"
+  },
+  {
     "revision": "938b0d10e0455112a9162df8df7316a8",
     "url": "/serhii_k/img/H-Tecnology.938b0d10.png"
+  },
+  {
+    "revision": "0f3d0d367113c417ad4e1a5e117a3d4e",
+    "url": "/serhii_k/img/LandX-min.0f3d0d36.png"
   },
   {
     "revision": "6e1e23274d67ee323dc5c489358ca468",
     "url": "/serhii_k/img/LandX.6e1e2327.png"
   },
   {
+    "revision": "476a88adb161efa7c86f0ff957030ff4",
+    "url": "/serhii_k/img/To-do-min.476a88ad.png"
+  },
+  {
     "revision": "03a79a62532a8210282f3c5d4ba2d735",
     "url": "/serhii_k/img/To-do.03a79a62.png"
+  },
+  {
+    "revision": "6b336bdfca20a396f63b4eecdf6a7889",
+    "url": "/serhii_k/img/alisayed-min.6b336bdf.png"
   },
   {
     "revision": "df2ef3614579c5e4e5f684e8d1f68cb2",
     "url": "/serhii_k/img/alisayed.df2ef361.png"
   },
   {
+    "revision": "73f0565b7096e08904f783c5f081ff2b",
+    "url": "/serhii_k/img/letusmake-min.73f0565b.png"
+  },
+  {
     "revision": "5ae5c2a210e45923ca07265472ac04e3",
     "url": "/serhii_k/img/letusmake.5ae5c2a2.png"
+  },
+  {
+    "revision": "98a7ba5bf41e40fdcc6681685e3f1152",
+    "url": "/serhii_k/img/vue-app-vuetify-min.98a7ba5b.png"
   },
   {
     "revision": "cd574f5defb87038ebcfa879a6da37a0",
     "url": "/serhii_k/img/vue-app-vuetify.cd574f5d.png"
   },
   {
-    "revision": "afd0283b9ae0565223c75064df282249",
+    "revision": "070cb04ed2c33311a16ebbe55875c1f8",
     "url": "/serhii_k/index.html"
   },
   {
-    "revision": "b1afe3d6198244c354d0",
-    "url": "/serhii_k/js/about.f0de3a78.js"
+    "revision": "a4faad2e534a2d8e1714",
+    "url": "/serhii_k/js/about.a9a9701c.js"
   },
   {
-    "revision": "cd8b362e446bdfa7cdb5",
-    "url": "/serhii_k/js/app.880f68c6.js"
+    "revision": "742110297c97248e111b",
+    "url": "/serhii_k/js/app.81dd52d1.js"
   },
   {
     "revision": "35031730657b2378e20f",
